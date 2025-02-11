bdbd1123f0d57d0dcdc6819cb9714d3807657bed
Wakaba <wakaba@suikawiki.org>
2025-02-11 22:29:14 +0900

    Made worker count configurable

--- bin/sarze-server.pl
+++ bin/sarze-server.pl
@@ -8,11 +8,12 @@ my $port = shift or die "No port";
 Sarze->run (
   hostports => [['0', $port]],
   psgi_file_name => path (__FILE__)->parent->child ('server.psgi'),
+  max_worker_count => $ENV{SARZE_MAX_WORKER_COUNT},
 )->to_cv->recv;
 
 =head1 LICENSE
 
-Copyright 2016-2017 Wakaba <wakaba@suikawiki.org>.
+Copyright 2016-2025 Wakaba <wakaba@suikawiki.org>.
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
